87582aba133126898c9b6dd3524fd486ba5fab89
S D Keerthiga Devi <132081378+S-D-Keerthiga-Devi@users.noreply.github.com>
2025-07-13 15:18:25 +0000

    Admin Dashboard

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -3,12 +3,17 @@ import {useParams} from 'react-router-dom'
 import { assets, blog_data, comments_data } from '../assets/assets'
 import Navbar from '../components/Navbar'
 import Moment from 'moment'
+import Footer from '../components/Footer'
+import Loader from '../components/Loader'
 
 const Blog = () => {
   const {id} = useParams()
   const [data, setData] = useState(null)
 
   const [comments, setComments] = useState([])
+
+  const [name, setName] = useState('')
+  const [content, setContent] = useState('')
 
   const fetchBlogData = async () =>{
     const data = blog_data.find(item => item._id === id)
@@ -18,6 +23,10 @@ const Blog = () => {
   const fetchComments = async ()=> {
     setComments(comments_data)
   }
+
+   const addComment = async (e)=>{
+      e.preventDefault();
+   }
 
 useEffect(()=>{
   fetchBlogData()
@@ -63,12 +72,29 @@ useEffect(()=>{
           {/* Add comment section */}
           <div className='max-w-3xl mx-auto'>
             <p className='font-semibold mb-4'>Add your comment</p>
+            <form onSubmit={addComment} className='flex flex-col items-start gap-4 max-w-lg'>
+              <input onChange={(e)=> setName(e.target.value)} value={name} type="text" placeholder='Name' required className='w-full p-2 border border-gray-300 rounded outline-none'/>
 
+              <textarea onChange={(e)=> setContent(e.target.value)} value={content} placeholder='Comment' className='w-full p-2 border border-gray-300 rounded outline-none h-48' required></textarea>
+
+              <button type='submit' className='bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer'>Submit</button>
+            </form>
+          </div>
+
+        {/* Share buttons */}
+          <div className='my-24 max-w-3xl mx-auto'>
+            <p className='font-semibold my-4'>Share this article on social media</p>
+            <div className='flex'>
+              <img src={assets.facebook_icon} alt="" />
+              <img src={assets.twitter_icon} alt="" />
+              <img src={assets.googleplus_icon} alt="" />
+            </div>
           </div>
       </div>
+      <Footer/>
 
     </div>
-  ) : <div>Loading...</div>
+  ) : <Loader/>
 }
 
 export default Blog
